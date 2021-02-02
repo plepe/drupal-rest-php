d525098d8c35e0da3e7ca38b9d2c8e27d66dbced
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2021-02-02 15:09:53 +0100

    drupal_paragraph_get()

--- drupal.php
+++ drupal.php
@@ -126,3 +126,32 @@ function drupal_paragraph_save ($drupal, $id, $content) {
 
   return $result;
 }
+
+function drupal_paragraph_get ($drupal, $id) {
+  $current_node = null;
+
+  $ch = curl_init();
+
+  curl_setopt($ch, CURLOPT_URL, "{$drupal['url']}/entity/paragraph/{$id}?_format=json");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    'Content-type: application/json',
+  ]);
+  curl_setopt($ch, CURLOPT_USERPWD, "{$drupal['user']}:{$drupal['pass']}");
+  curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+  $result = curl_exec($ch);
+  if ($result[0] !== '{') {
+    print $result;
+    exit(1);
+  }
+
+  $result = json_decode($result, true);
+
+  if (!array_key_exists('id', $result)) {
+    print_r($result);
+    exit(1);
+  }
+
+  return $result;
+}
